fbaea1739a4ae646f5e4e2e850b7fd0d0336b5c9
Spyke Lionel <social.lionel@gmail.com>
2025-07-23 16:05:15 +0100

    add all files and dirs

---
+++ scripts/header.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+// const headerHeight = header.offsetHeight;
+const headerContent = `
+  <nav class="header__container">
+    <div class="header__logo">
+      <a href="index.html">
+        <img src="./../images/img1.jpg" class="img1" alt="Logo">
+      </a>
+    </div>
+    <div class="header__links">
+      <ul class="header__list">
+        <li class="header__item"><a href="index.html">Home</a></li>
+        <li class="header__item"><a href="about.html">About</a></li>
+        <li class="header__item"><a href="contact.html">Contact</a></li>
+        <li class="header__item"><a href="gallery.html">Gallery</a></li>
+      </ul>
+    </div>
+    </nav>
+
+`;
+
+header.innerHTML = headerContent;
